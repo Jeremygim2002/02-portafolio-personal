df53c54e114c40380ddc870f4b4f18f4414cec2a
Jeremygim2002 <127067985+Jeremygim2002@users.noreply.github.com>
2025-08-30 17:26:17 -0500

    agregar origen aceptado para CORS en el middleware

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,7 +2,8 @@ import cors from 'cors';
 
 const ACCEPTED_ORIGINS = new Set([
     'http://localhost:3000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://02-portafolio-personal-frontend.vercel.app/'
 ]);
 
 export const corsMiddleware = cors({
